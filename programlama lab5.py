335b2c4435931aa010a96538200066056f458f9a
ravasiyeg <44299635+ravasiyeg@users.noreply.github.com>
2020-03-09 21:46:06 +0200

    Add files via upload

---
+++ programlama lab5.py
@@ -0,0 +1,78 @@
+List_1=[0,5,25,100,5,5,0,100]
+
+def my_h(List_1):
+    my_d=dict()
+    for i in List_1:
+        if i in my_d:
+            my_d(i)=my_d[i]+1
+        else:
+            my_d(i)=my_d[i]+1
+    return my_d
+print(my_h(List_1))
+
+
+
+def lookup(d,v):
+    for k in d:
+        if d[k]==v:
+            return k
+    return-1
+
+
+def fibo_rec(n):
+    if n<2:
+        return n
+    else:
+        fibo_rec(n-1)+fibo_rec(n-2)
+
+known={0:0,1:1}
+def fibo_rec(n):
+    if n in known:
+        return known[n]
+    else:
+        result=fibo_rec(n-1)+fibo_rec(n-2)
+        known[n]=result
+        return result
+
+from synpy import FiniteSet
+s=FiniteSet(1,1,5,Fraction(1,5))
+for member in j:
+    print(member)
+    t=FiniteSet(Fraction(1,5))
+    #if s==t:
+        
+
+#list_1=[1,4,7,84,3,62,23]
+
+
+my_d=dict()
+my_d={1:'bir',2:2,'3':'three'}
+
+#my_d
+for key in my_d.keys():
+    print(key,my_d[key]
+def my_h(List_1):
+    my_d={}
+        for item in List_1:
+            if item not in my_d:
+                  my_d[item]=1
+              else:
+                  my_d[item]=my_d[item]+1
+          return my_d
+print(my_h([2,3,4,6,2,5,6,6,6,6,6,2]))
+
+
+def lookup(d,v):
+          for key in d:
+              if d[key]==v:
+                  return key
+              else:
+                  return-1
+lookup(my_d,2)
+
+
+
+    
+
+          
+          
